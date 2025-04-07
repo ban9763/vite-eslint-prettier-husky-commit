5e5adde2512a3a2033e8cc3c0b961030ef1b04a5
ban9763 <2248993992@qq.com>
2025-04-07 16:20:47 +0800

    build: docker nginx 1.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
 import path from 'path'; //引入path用到了上面安装的@types/node
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    // 复制到
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'nginx/Dockerfile',
+          dest: '.',
+        },
+        {
+          src: 'nginx/docker.nginx.conf',
+          dest: '.',
+        },
+      ],
+    }),
+  ],
   //路径别名配置如下
   resolve: {
     alias: {
-      '@': path.resolve('./src') // 用@代替src
-    }
-  }
-})
+      '@': path.resolve('./src'), // 用@代替src
+    },
+  },
+});
